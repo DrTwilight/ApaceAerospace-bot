accc1e7498b641246937c507723e10b00f1be325
grey <greysilly7@gmail.com>
2020-08-14 15:03:25 -0400

    dog and furry command

---
+++ src/commands/furry.js
@@ -0,0 +1,20 @@
+exports.run = async (client, message, args, level) => {
+    const num = Math.floor(Math.random() * 100000);
+    return message.say(`Fursona #${num}`, {
+        files: [`https://thisfursonadoesnotexist.com/v2/jpgs/seed${num.toString().padStart(5, '0')}.jpg`]
+    });
+  };
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: "User"
+  };
+  
+  exports.help = {
+    name: "furry",
+    category: "Fun",
+    description: "Responds with a randomly generated fursona.",
+    usage: "furry"
+  };
