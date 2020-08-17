c901d68625c802ad5f5db9cba711f2c968022bbd
Scott Gould <greysilly7@gmail.com>
2020-08-17 13:47:23 -0700

    fixed furry comamdn?

--- src/commands/furry.js
+++ src/commands/furry.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args, level) => {
     const num = Math.floor(Math.random() * 100000);
-    return message.say(`Fursona #${num}`, {
+    return message.send(`Fursona #${num}`, {
         files: [`https://thisfursonadoesnotexist.com/v2/jpgs/seed${num.toString().padStart(5, '0')}.jpg`]
     });
   };
