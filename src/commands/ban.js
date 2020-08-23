1161d8157d3380db228b56aeb585278ff759ef09
DrTwilight <greysilly7@gmail.com>
2020-08-22 18:14:05 -0700

    A bunch of stuff

---
+++ src/commands/ban.js
@@ -0,0 +1,28 @@
+exports.run = async (client, message, args) => {
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.bannable) 
+      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+    
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+};
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: [],
+    permLevel: "Moderator"
+  };
+  
+  exports.help = {
+    name: "ban",
+    category: "Moderation",
+    description: "It zaps a person off the channel forever.",
+    usage: "ban <user>"
+  };
--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -2,4 +2,6 @@ module.exports = (client, guild) => {
   if (client.settings.has(guild.id)) {
     client.settings.delete(guild.id);
   }
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.cache.size} servers`);
 }
