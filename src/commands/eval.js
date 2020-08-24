0633643414beaa62c01f0c477f8a39f32fbdf40c
DrTwilight <greysilly7@gmail.com>
2020-08-24 12:11:15 -0700

    Fixed ping added eval and hug commands

---
+++ src/commands/eval.js
@@ -0,0 +1,45 @@
+exports.run = async (client, message, args, _level) => { // eslint-disable-line no-unused-vars
+    const code = args.join(' ');
+    try {
+      /*
+        const {NodeVM} = require('vm2');
+        const vm = new NodeVM({
+            sandbox: {},
+            require: {
+                external: true,
+                builtin: ['fs', 'path'],
+                root: "./",
+                mock: {
+                    fs: {
+                        readFileSync() { return 'Nice try!'; }
+                    }
+                }
+            },
+        });
+      */
+    const {VM} = require('vm2');
+    const vm = new VM({
+        timeout: 2500,
+        sandbox: {}, 
+    });
+      const evaled = vm.run(code);
+      const clean = await client.clean(client, evaled);
+      message.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${await client.clean(client, err.message)}\n\`\`\``);
+    }
+  };
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 'User',
+  };
+  
+  exports.help = {
+    name: 'eval',
+    category: 'System',
+    description: 'Evaluates arbitrary javascript.',
+    usage: 'eval [...code]',
+  };
