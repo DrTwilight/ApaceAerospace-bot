c360422dbef0c0250f190159dcec70d0f3e159ce
Dr.T <greysilly7@gmail.com>
2020-09-01 22:11:22 -0400

    idk

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -4,4 +4,4 @@ module.exports = (client, guild) => {
   }
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
   client.user.setActivity(`Serving ${client.guilds.cache.size} servers`);
-}
+};
