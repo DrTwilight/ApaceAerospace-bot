9a547473a64eeaf1507d497c4cd31deed26fdf47
Dr.T <greysilly7@gmail.com>
2020-09-09 11:10:14 -0400

    fixed a error

--- src/Bot.js
+++ src/Bot.js
@@ -21,36 +21,34 @@ client.aliases = new Collection();
 
 const init = async () => {
   try {
-  }
-  const cmdFiles = await readdir('./src/commands');
-  console.log(`Loading a total of ${cmdFiles.length} commands.`);
-  cmdFiles.forEach((f) => {
-    if (!f.endsWith('.js')) return;
-    const response = client.loadCommand(f);
-    if (response) console.log(response);
-  });
+    const cmdFiles = await readdir('./src/commands');
+    console.log(`Loading a total of ${cmdFiles.length} commands.`);
+    cmdFiles.forEach((f) => {
+      if (!f.endsWith('.js')) return;
+      const response = client.loadCommand(f);
+      if (response) console.log(response);
+    });
 
-  const evtFiles = await readdir('./src/events');
-  console.log(`Loading a total of ${evtFiles.length} events.`);
-  evtFiles.forEach((file) => {
-    const eventName = file.split('.')[0];
-    console.log(`Loading Event: ${eventName}`);
-    const event = require(`./events/${file}`);
-    client.on(eventName, event.bind(null, client));
-  });
+    const evtFiles = await readdir('./src/events');
+    console.log(`Loading a total of ${evtFiles.length} events.`);
+    evtFiles.forEach((file) => {
+      const eventName = file.split('.')[0];
+      console.log(`Loading Event: ${eventName}`);
+      const event = require(`./events/${file}`);
+      client.on(eventName, event.bind(null, client));
+    });
 
-  // Generate a cache of client permissions for pretty perm names in commands.
-  client.levelCache = {};
-  for (let i = 0; i < client.config.permLevels.length; i++) {
-    const thisLevel = client.config.permLevels[i];
-    client.levelCache[thisLevel.name] = thisLevel.level;
-  }
+    // Generate a cache of client permissions for pretty perm names in commands.
+    client.levelCache = {};
+    for (let i = 0; i < client.config.permLevels.length; i++) {
+      const thisLevel = client.config.permLevels[i];
+      client.levelCache[thisLevel.name] = thisLevel.level;
+    }
 
-  client.login(client.config.token);
+    client.login(client.config.token);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-  
 };
 
 init();
