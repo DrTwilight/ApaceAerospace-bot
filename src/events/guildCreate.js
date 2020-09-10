c1bc0162e213a3e097a053378eb8a8a11d8a171b
Dr.T <greysilly7@gmail.com>
2020-09-10 16:17:45 -0400

    Rewrite basis

--- src/events/guildCreate.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable max-len */
-
-module.exports = (client, guild) => {
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.cache.size} servers`);
-};
